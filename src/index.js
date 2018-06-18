775c10ce8b777609d67bfb8e19dd2aa98affbfd1
Johnny Hsiao <johnnyhsiao87@gmail.com>
2018-06-17 23:05:39 -0400

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import BarcodeScanner from './BarcodeScanner';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<BarcodeScanner />, document.getElementById('root'));
-registerServiceWorker();
